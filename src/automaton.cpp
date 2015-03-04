c8821e99adc6b0a56d5e8d5c76906633a8ef6fc8
Samuel MAGNAN <samuel.magnan@insa-lyon.fr>
2015-03-04 09:33:45 +0100

    automaton files structure

---
+++ src/automaton.cpp
@@ -0,0 +1,21 @@
+// ---------------------------------------------
+//  Automaton.h
+//
+//	Created	 :
+//		by 	 : Samuel MAGNAN
+//		date : 04/03/2015
+//
+// ---------------------------------------------
+
+#include "automaton.h"
+
+// Automaton constructor
+Automaton::Automaton() 
+{
+
+}
+
+Automaton::~Automaton()
+{
+	
+}
---
+++ src/automaton.h
@@ -0,0 +1,22 @@
+// ---------------------------------------------
+//  Automaton.h
+//
+//	Created	 :
+//		by 	 : Samuel MAGNAN
+//		date : 04/03/2015
+//
+// ---------------------------------------------
+
+#ifndef AUTOMATON_H
+#define AUTOMATON_H
+ 
+class Automaton
+{
+private:
+protected:
+public:
+    Automaton();
+    virtual ~Automaton();
+};
+ 
+#endif
