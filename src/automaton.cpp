45dca04113539ff01c79c4b814b90e37b962c09c
LilCartell <lilian.cartellier01@orange.fr>
2015-03-11 11:36:52 +0100

    Started automaton implementation

--- src/automaton.cpp
+++ src/automaton.cpp
@@ -12,15 +12,41 @@
 // Automaton constructor
 Automaton::Automaton() 
 {
-
 }
 
 Automaton::~Automaton()
 {
+	Symbol* currentSymbol;
+	State* currentState;
+	
+	while(symbolStack.empty())
+	{
+		currentSymbol = symbolStack.pop();
+		delete currentSymbol;
+	}
+	
+	while(symbolState.empty())
+	{
+		currentState = symbolState.pop();
+		delete currentState;
+	}
+}
 
+void Automaton::read()
+{
+	//Prends le prochain jeton (voir si l'automate a déjà toute la liste ou pas)
+	//Appel la transition de l'état courant (sommet de la pile) avec le symnbole lu
 }
 
 void Automaton::shift(Symbol * symbol, State * state)
 {
-    
+    symbolStack.push(symbol);
+    stateStack.push(symbol);
+}
+
+void Automaton::reduce(int numeroRegle)
+{
+	//Depiler les symboles à droite de la règle
+	//Ajouter l'instruction qui va correspond
+	//Passer à l'état qui correspond au symbol non terminal
 }
