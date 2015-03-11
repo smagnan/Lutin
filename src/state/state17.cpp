8f9e83ae02572d67a0d86b12634f48654e3e8c09
Pierre Godard <pierre.godard@insa-lyon.fr>
2015-03-11 11:05:42 +0100

    Added reduce management in state generation

--- src/state/state17.cpp
+++ src/state/state17.cpp
@@ -26,6 +26,14 @@ bool State17::transition(Automaton & automaton, Symbol * s)
     switch(*s)
     {
 
+        case PV:
+            automaton.reduce(5);
+            break;
+
+        case VIR:
+            automaton.reduce(5);
+            break;
+
     }
     
     return false;
--- src/state/state43.cpp
+++ src/state/state43.cpp
@@ -26,6 +26,30 @@ bool State43::transition(Automaton & automaton, Symbol * s)
     switch(*s)
     {
 
+        case PLUS:
+            automaton.reduce(16);
+            break;
+
+        case MINUS:
+            automaton.reduce(16);
+            break;
+
+        case MULT:
+            automaton.reduce(16);
+            break;
+
+        case DIVIDE:
+            automaton.reduce(16);
+            break;
+
+        case CLOSEBY:
+            automaton.reduce(16);
+            break;
+
+        case PV:
+            automaton.reduce(16);
+            break;
+
     }
     
     return false;
