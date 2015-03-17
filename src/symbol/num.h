6ae793852eb7289ff4d0ceac75184d518dd0aab3
samuel <sam.samuel.magnan@gmail.com>
2015-03-17 15:40:26 +0100

    Merge correction

--- src/symbol/num.h
+++ src/symbol/num.h
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-#include "symbol.h" 
-#include "expr.h"
-=======
 #ifndef NUM_H
 #define NUM_H
 
 #include "expr.h" 
->>>>>>> 041767b64e9041dd383406552ad9c3487693e0dd
 
 class Num : public Expr
 {
