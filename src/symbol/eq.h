1c146168645157bce8eea6f46e9e332a9da566ae
Pierre Godard <pierre.godard@insa-lyon.fr>
2015-03-17 15:28:16 +0100

    Added corrected symbol files

---
+++ src/symbol/eq.h
@@ -0,0 +1,24 @@
+#ifndef EQ_H
+#define EQ_H
+
+#include "symbol.h" 
+
+class Eq : public Symbol
+{
+
+	public:
+
+		//Default Constructor
+		Eq();
+
+		//Deconstructor
+		~Eq();
+
+
+	private:
+
+		//Disable the following by Eq 
+
+};
+
+#endif
