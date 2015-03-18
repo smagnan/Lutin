2902d6f7e9d9fd91e9a0d6729284549c0824da51
samuel <sam.samuel.magnan@gmail.com>
2015-03-18 13:14:00 +0100

    utils fix

--- src/utils.cpp
+++ src/utils.cpp
@@ -7,11 +7,11 @@ std::string Utils::doubleToString(double val)
 	return strs.str();
 }
 
-double Utils::stringToDouble(std:: str)
+double Utils::stringToDouble(std::string str)
 {
     std::istringstream i(str);
     double ret;
-    if (!(i>>x))
+    if (!(i>>ret)) // TODO it was !(i>>x) => not working, so I changed x to ret
         return 0;
     return ret;
 }
